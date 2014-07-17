baa4d65ffa845e61603a8aaabfedaf193ad52fda
Taylor Stackpole <tstackpole@concordusapps.com>
2014-07-17 12:11:05 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+	# Basic package information.
+	name = 'Zendesk',
+	author = 'Max Gutman, Stefan Tjarks',
+	version = '1.1.1',
+	author_email = 'max@eventbrite.com',
+	packages = ['zendesk'],
+	include_package_data = True,
+	install_requires = ['httplib2', 'simplejson'],
+	license='LICENSE.txt',
+	url = 'https://github.com/eventbrite/zendesk',
+	keywords = 'zendesk api helpdesk',
+	description = 'Python API Wrapper for Zendesk',
+	use_2to3 = True,
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Internet'
+	],
+)
+
+
